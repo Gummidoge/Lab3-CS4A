993730dada94c2c87c7a2ab82c9ae940b2f657b2
Audrey Woodham <gummidoge@gmail.com>
2026-09-21 20:40:10 -0700

    Added prompt for first and last name

--- Lab3.java
+++ Lab3.java
@@ -4,11 +4,13 @@ public class Lab3 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 
-		// Prompt user for student name
-		System.out.print("Enter student name: ");
-		String studentName = input.nextLine();
+		// Prompt user for first and last name
+		System.out.print("Enter student's first name: ");
+		String firstName = input.next();
+		System.out.print("Enter student's last name: ");
+		String lastName = input.next();
 
-		// Output student name
-		System.out.println("Hello " + studentName + "!");
+		// Output student first and last name
+		System.out.println("Hello " + firstName + " " + lastName + "!");
 	}
 }
